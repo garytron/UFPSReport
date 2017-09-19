2dd89252e2bd4d4572331ad817244f00ac669194
AngelAparicioUfps <32025485+AngelAparicioUfps@users.noreply.github.com>
2017-09-19 08:46:19 -0500

    Reportes

---
+++ app/src/main/java/Util/Reporte.java
@@ -0,0 +1,85 @@
+package Util;
+
+import java.util.Date;
+
+/**
+ * Created by acer on 18/09/2017.
+ */
+
+public class Reporte {
+    private Integer id_reporte;
+    private Integer clase;
+    private String estado;
+    private Date fecha;
+    private String instrumento;
+
+    public Integer getClase() {
+        return clase;
+    }
+
+    public void setClase(Integer clase) {
+        this.clase = clase;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getInstrumento() {
+        return instrumento;
+    }
+
+    public void setInstrumento(String instrumento) {
+        this.instrumento = instrumento;
+    }
+
+    public String getInstrumentoNombre() {
+        return instrumentoNombre;
+    }
+
+    public void setInstrumentoNombre(String instrumentoNombre) {
+        this.instrumentoNombre = instrumentoNombre;
+    }
+
+    public String getTipoDescripcion() {
+        return tipoDescripcion;
+    }
+
+    public void setTipoDescripcion(String tipoDescripcion) {
+        this.tipoDescripcion = tipoDescripcion;
+    }
+
+    private String instrumentoNombre;
+    private String tipoDescripcion;
+
+    public Integer getId_reporte() {
+        return id_reporte;
+    }
+
+    public void setId_reporte(Integer id_reporte) {
+        this.id_reporte = id_reporte;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Reporte Nro=" + id_reporte +
+                ", Fecha=" + fecha.getDay()+"-"+fecha.getHours()+":"+fecha.getMinutes() +
+                ", Instrumento='" + instrumento + '\'' +
+                " Nro='" + instrumentoNombre + '\'' +
+                ", tipoDescripcion='" + tipoDescripcion + '\''
+                ;
+    }
+}
--- app/src/main/java/com/example/cristianramirez/ufpsreport/ValidarIncidente.java
+++ app/src/main/java/com/example/cristianramirez/ufpsreport/ValidarIncidente.java
@@ -1,12 +1,15 @@
 package com.example.cristianramirez.ufpsreport;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
-import org.json.JSONArray;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -15,11 +18,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
+import java.util.List;
 
-public class ValidarIncidente extends AppCompatActivity implements  View.OnClickListener {
+import Util.Reporte;
+
+public class ValidarIncidente extends AppCompatActivity implements View.OnClickListener {
     CheckBox el1, el2, el3, el4, el5, el6, el7, el8;
     Button btnEliminar, btnAprobar;
+    String resultado = "";
+    String accion = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,71 +42,136 @@ public class ValidarIncidente extends AppCompatActivity implements  View.OnClick
         el6 = (CheckBox) findViewById(R.id.checkBox6);
         el7 = (CheckBox) findViewById(R.id.checkBox7);
         el8 = (CheckBox) findViewById(R.id.checkBox8);
+
         btnAprobar.setOnClickListener(this);
         btnEliminar.setOnClickListener(this);
+
+        limpiarC();
+
+
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.btnAprobar) {
-
+            accion = "";
+            AccesoRemoto a = new AccesoRemoto();
+            a.execute();
+            limpiarC();
+            llenarChecs();
         }
         if (view.getId() == R.id.btnEliminar) {
-
+            accion = "";
+            AccesoRemoto a = new AccesoRemoto();
+            a.execute();
+            limpiarC();
+            llenarChecs();
         }
+
     }
+
 
     public void llenarChecs() {
-        final String respuesta = generarJson();
-        ArrayList<Object> muestreo = new ArrayList<>();
-        try {
-            JSONArray jsons = new JSONArray(respuesta);
-            for(int i = 0;i< jsons.length();i++){
-             muestreo.add(jsons.get(i));
-            }
-            for(int j=0;j<8;j++){
-            if(j==1){
-            }
-            }
 
-        }catch (Exception e ){
+        int con = 0;
+        accion = "/reporte/selectAll";
+        try {
+            AccesoRemoto a = new AccesoRemoto();
+            a.execute();
+
+            Gson miGson = new Gson();
+            List<Reporte> aux = miGson.fromJson(resultado, new TypeToken<List<Reporte>>() {
+            }.getType());
+            for (Reporte r : aux) {
+                if (con == 0) {
+                    el1.setEnabled(true);
+                    el1.setText(aux.get(con).toString());
+                }
+                if (con == 1) {
+                    el2.setEnabled(true);
+                    el2.setText(aux.get(con).toString());
+                }
+                if (con == 2) {
+                    el3.setEnabled(true);
+                    el3.setText(aux.get(con).toString());
+                }
+                if (con == 3) {
+                    el4.setEnabled(true);
+                    el4.setText(aux.get(con).toString());
+                }
+                if (con == 4) {
+                    el5.setEnabled(true);
+                    el5.setText(aux.get(con).toString());
+                }
+                if (con == 5) {
+                    el6.setEnabled(true);
+                    el6.setText(aux.get(con).toString());
+                }
+                if (con == 6) {
+                    el7.setEnabled(true);
+                    el7.setText(aux.get(con).toString());
+                }
+                if (con == 7) {
+                    el8.setEnabled(true);
+                    el8.setText(aux.get(con).toString());
+                }
+                con++;
+            }
+            con = 0;
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
- public void limpiarC(){
-     el1.setEnabled(false);
-     el2.setEnabled(false);
-     el3.setEnabled(false);
-     el4.setEnabled(false);
-     el5.setEnabled(false);
-     el6.setEnabled(false);
-     el7.setEnabled(false);
-     el8.setEnabled(false);
- }
 
-    public String generarJson() {
-        URL url = null;
-        String linea = "";
-        int respuesta = 0;
-        StringBuilder result = null;
 
-        try {
-            url = new URL("");
-            HttpURLConnection conection = (HttpURLConnection) url.openConnection();
-            respuesta = conection.getResponseCode();
-
-            result = new StringBuilder();
-            if (respuesta == HttpURLConnection.HTTP_OK) {
-                InputStream in = new BufferedInputStream(conection.getInputStream());
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
-                while ((linea = reader.readLine()) != null)
-                    result.append(linea);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return result.toString();
+    public void limpiarC() {
+        el1.setEnabled(false);
+        el2.setEnabled(false);
+        el3.setEnabled(false);
+        el4.setEnabled(false);
+        el5.setEnabled(false);
+        el6.setEnabled(false);
+        el7.setEnabled(false);
+        el8.setEnabled(false);
     }
+
+
+    private class AccesoRemoto extends AsyncTask<Void, Void, String> {
+
+        protected String doInBackground(Void... argumentos) {
+            URL url = null;
+            String linea = "";
+            int respuesta = 0;
+            StringBuilder result = null;
+
+            try {
+
+                url = new URL("http://gidis.ufps.edu.co:8088/servicios_arch" + accion);
+                HttpURLConnection conection = (HttpURLConnection) url.openConnection();
+                respuesta = conection.getResponseCode();
+
+                result = new StringBuilder();
+                if (respuesta == HttpURLConnection.HTTP_OK) {
+                    InputStream in = new BufferedInputStream(conection.getInputStream());
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                    while ((linea = reader.readLine()) != null)
+                        result.append(linea);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            resultado=s;
+
+           Toast.makeText(getApplicationContext(),"Cargando Datos", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
